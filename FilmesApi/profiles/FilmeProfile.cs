7a1db6185f77fe5e380746c273f1d7e068349ae4
gesse.macedo-sc <gesse.soares.macedo@gmail.com>
2022-08-16 22:10:49 -0300

    Add project files.

---
+++ FilmesApi/profiles/FilmeProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using FilmesApi.Data.Dtos;
+using FilmesApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesApi.profiles
+{
+    public class FilmeProfile : Profile
+    {
+        public FilmeProfile()
+        {
+            CreateMap<CreateFilmeDto, Filme>();
+            CreateMap<Filme, ReadFilmeDto>();
+            CreateMap<UpdateFilmeDto, Filme>();
+
+        }
+    }
+}
